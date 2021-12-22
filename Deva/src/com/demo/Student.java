58d83e0ee628f2a881b8f41076153a76d74f6b1c
Devang15Pirta <95690841+Devang15Pirta@users.noreply.github.com>
2021-12-23 00:26:35 +0530

    Add files via upload

---
+++ Deva/src/com/demo/Student.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Student {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+void display() {
+	System.out.println("Name is: "+name );
+}
+
+}
